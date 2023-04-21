4197bf9b3f9eb2e86569ae219ad8f90ed80dfb12
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-04-20 17:38:13 -0700

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -22,3 +22,11 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a good idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
